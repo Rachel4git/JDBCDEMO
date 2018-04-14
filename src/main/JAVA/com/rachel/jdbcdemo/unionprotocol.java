61609810941cb6e5d98278aa0f6b805101c720ae
Rachel4git <780169863@qq.com>
2018-04-14 21:03:09 +0800

    2018041421 02

---
+++ src/main/JAVA/com/rachel/jdbcdemo/unionprotocol.java
@@ -0,0 +1,92 @@
+package com.rachel.jdbcdemo;
+
+/**
+ * Created by hd48552 on 2018/4/14.
+ */
+public class unionprotocol {
+
+        // ��ˮ��
+        private long id;
+        // ���Ե�����
+        private String airline;
+        // ���֤��
+        private int union_airline_amount;
+        // ׼��֤��
+        private String union_airlines;
+        // ѧ����
+        private java.sql.Timestamp gmt_create;
+        // ѧ����ַ
+        private java.sql.Timestamp gmt_modified;
+        // ���Է���.
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(int id) {this.id = id;}
+
+        public String getAriline() {
+        return airline;
+    }
+
+        public  void  setAriline(String airline){ this.airline= airline;}
+
+        public int getUnion_airline_amount() {
+            return union_airline_amount;
+        }
+
+        public void setUnion_airline_amount(int union_airline_amount) {
+            this.union_airline_amount = union_airline_amount;
+        }
+
+        public String getUnion_airlines() {
+            return union_airlines;
+        }
+
+        public void setUnion_airlines(String union_airlines) {
+            this.union_airlines = union_airlines;
+        }
+
+        public java.sql.Timestamp getGmt_create() {
+            return gmt_create;
+        }
+
+        public void setGmt_create(java.sql.Timestamp gmt_create) {
+            this.gmt_create = gmt_create;
+        }
+
+        public java.sql.Timestamp getGmt_modified() {
+            return gmt_modified;
+        }
+
+        public void setGmt_modified(java.sql.Timestamp gmt_modified) {
+            this.gmt_modified = gmt_modified;
+        }
+
+
+        public unionprotocol(long id, int union_airline_amount, String airline, String union_airlines,
+                             java.sql.Timestamp gmt_create, java.sql.Timestamp gmt_modified) {
+            super();
+            this.id = id;
+            this.union_airline_amount = union_airline_amount;
+            this.airline = airline;
+            this.union_airlines = union_airlines;
+            this.gmt_create = gmt_create;
+            this.gmt_modified = gmt_modified;
+        }
+
+        public unionprotocol() {
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public String toString() {
+            return "unionprotocol [id=" + id + ", union_airline_amount=" + union_airline_amount + ", airline="
+                    + airline + ", union_airlines=" + union_airlines + ", gmt_create="
+                    + gmt_create + ", gmt_modified=" + gmt_modified
+                    + "]\n";
+
+
+    }
+
+}
